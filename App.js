d8d9689dbf005bf84361fd29d4b7ec5d7fc93770
Andreas Zetterlund <andreas.zetterlund1@hitachi-powergrids.com>
2021-06-22 15:11:09 +0200

    Init

--- App.js
+++ App.js
@@ -1,42 +1,131 @@
-import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet} from 'react-native';
 import {
   ViroARScene,
   ViroText,
   ViroConstants,
-  ViroARSceneNavigator
+  ViroARSceneNavigator,
+  ViroARTrackingTargets,
+  ViroARImageMarker,
+  ViroBox,
+  Viro3DObject, 
+  ViroAmbientLight,
+  ViroMaterials,
 } from '@viro-community/react-viro';
 
 class HelloWorldSceneAR extends Component {
-
   constructor(props) {
     super(props);
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+      text: 'Initializing AR...',
+      texture: "white",
+      playAnim: false,
+      animateCar: false,
+      tapWhite: false,
+      tapBlue: false,
+      tapGrey: false,
+      tapRed: false,
+      tapYellow: false,
     };
+
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
+    ViroARTrackingTargets.createTargets({
+      "targetOne" : {
+        source : require('./res/qrcode.jpeg'),
+        orientation : "Up",
+        physicalWidth : 0.25 // real world width in meters
+      }
+    });
   }
-
+  /* <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        />*/
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      <ViroARScene onTrackingUpdated={this._onInitialized} autofocus={true}>
+        <ViroARImageMarker target={'targetOne'}>
+        <Viro3DObject
+            scale={[0.1,0.1, 0.1]}
+            source={require('./res/tesla/object_car.obj')}
+            resources={[require('./res/tesla/object_car.mtl'),
+                        ]}
+            type="OBJ"
+            materials={this.state.texture}
+            onClick={this._toggleButtons} />
+        </ViroARImageMarker>
+        <ViroAmbientLight color="#FFFFFF" />
       </ViroARScene>
     );
   }
+//                materials={["heart"]}
 
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Hello World!"
+        text: 'Hello Worldlll!',
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
   }
 }
+ViroMaterials.createMaterials({
+  white: {
+    lightingModel: "PBR",
+    diffuseTexture: require('./res/tesla/object_car_main_Base_Color.png'),
+    metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
+    roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
+  },
+  blue: {
+    lightingModel: "PBR",
+    diffuseTexture: require('./res/tesla/object_car_main_Base_Color_blue.png'),
+    metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
+    roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
+  },
+  grey: {
+    lightingModel: "PBR",
+    diffuseTexture: require('./res/tesla/object_car_main_Base_Color_grey.png'),
+    metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
+    roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
+  },
+  red: {
+    lightingModel: "PBR",
+    diffuseTexture: require('./res/tesla/object_car_main_Base_Color_red.png'),
+    metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
+    roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
+  },
+  yellow: {
+    lightingModel: "PBR",
+    diffuseTexture: require('./res/tesla/object_car_main_Base_Color_yellow.png'),
+    metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
+    roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
+  },
+  white_sphere: {
+    lightingModel: "PBR",
+    diffuseColor: "rgb(231,231,231)",
+  },
+  blue_sphere: {
+    lightingModel: "PBR",
+    diffuseColor: "rgb(19,42,143)",
+  },
+  grey_sphere: {
+    lightingModel: "PBR",
+    diffuseColor: "rgb(75,76,79)",
+  },
+  red_sphere: {
+    lightingModel: "PBR",
+    diffuseColor: "rgb(168,0,0)",
+  },
+  yellow_sphere: {
+    lightingModel: "PBR",
+    diffuseColor: "rgb(200,142,31)",
+  },
+});
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
@@ -48,16 +137,16 @@ var styles = StyleSheet.create({
   },
 });
 
-export default class App extends React.Component{
-	render(){
-			return(
-				<ViroARSceneNavigator
-					autofocus={true}
-					initialScene={{
-						scene: HelloWorldSceneAR,
-					}}
-					style={{flex: 1}}
-				/>
-			);
-		}
+export default class App extends React.Component {
+  render() {
+    return (
+      <ViroARSceneNavigator
+        autofocus={true}
+        initialScene={{
+          scene: HelloWorldSceneAR,
+        }}
+        style={{flex: 1}}
+      />
+    );
+  }
 }
